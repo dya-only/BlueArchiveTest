8458f2348434e646ed728465082d1640d3412891
dya-code <dyacode@proton.me>
2022-12-25 21:21:37 +0900

    edit margin

--- src/views/PickUp.tsx
+++ src/views/PickUp.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMusic } from '@fortawesome/free-solid-svg-icons'
 
 import Atsuko_bg from "../assets/atsuko.png"
+import Sub_bg from "../assets/sub_bg.png"
 
 import connected_sky from "./connected_sky.wav"
 import Atsuko_PV from "./atsuko_bg.mp4"
@@ -15,7 +16,9 @@ function PickUp() {
   }
 
   return (
-    <div className="pick-up bg-cover font-molu-bold">
+    <div className="pick-up bg-cover font-molu-bold overflow-x-hidden">
+
+    <img className='w-screen h-screen fixed -z-20' src={ Sub_bg } alt="" />
 
       { isMusic == true ? 
         <audio loop autoPlay>
@@ -28,14 +31,16 @@ function PickUp() {
         <source src={ Atsuko_PV } type="video/mp4" />
       </video>
 
+      {/* Music Button */}
       <div className="flex justify-end">
-        <button className='fixed mr-4 mt-20 bg-white p-3 w-12 h-12 flex justify-center items-center drop-shadow-xl rounded-lg' onClick={onMusic}>
+        <button className='fixed mr-8 mt-20 bg-white p-3 w-12 h-12 flex justify-center items-center drop-shadow-xl rounded-lg' onClick={onMusic}>
           <FontAwesomeIcon className='text-3xl text-[#436299]' icon={faMusic} />
         </button>
       </div>
 
-      <div className="flex justify-end items-center pr-8 w-screen h-screen text-[#383f46]">
-        <div className="window-bg fixed z-20 w-[650px] h-[570px] bg-[#a9e0f5] rounded-md shadow-md">
+      {/* Pick Up Window */}
+      <div className="flex justify-end items-center w-screen h-screen text-[#383f46]">
+        <div className="window-bg fixed z-20 w-[650px] h-[570px] bg-[#a9e0f5] rounded-md shadow-md mr-8">
           <div className="pt-3 pb-3 flex justify-center items-center w-[150px] m-[8px] bg-[#f1fbfc] rounded-t-md font-molu text-[23px]">픽업 모집</div>
           
           <div className="info w-[650px] h-[430px] -mt-[12px] bg-[#f1fbfc]">
