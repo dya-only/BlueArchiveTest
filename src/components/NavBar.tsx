3c1e5f9036a5e38e306513fa24cb39503bcaa717
dya-code <dyacode@proton.me>
2022-12-25 23:31:41 +0900

    button & click effect

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,12 +14,12 @@ function NavBar() {
         <img className="h-[60px]" src={ nav } alt="" />
 
         <div className="first fixed flex items-center justify-center">
-          <button className="prev z-10 bg-[#456399] rounded-full w-[70px] h-[70px] m-10 mt-[85px] transition duration-300 hover:opacity-70">
+          <button className="prev transition duration-100 active:scale-90 z-10 bg-[#456399] rounded-full w-[70px] h-[70px] m-10 mt-[85px] transition duration-300 hover:opacity-70">
             <FontAwesomeIcon className='text-5xl text-white' icon={faArrowLeft} />
           </button>
 
           <div className="title text-[#2c4663] tracking-tight text-4xl border-b-4 h-[47px] -ml-12 mt-3 pl-8 border-[#ffe03d]">학생 모집</div>
-          <button className="help text-white bg-[#2a4566] font-bold text-xl w-8 h-8 ml-2 mt-1 rounded-md transition duration-300 hover:opacity-70">?</button>
+          <button className="help transition duration-100 active:scale-90 text-white bg-[#2a4566] font-bold text-xl w-8 h-8 ml-2 mt-1 rounded-md transition duration-300 hover:opacity-70">?</button>
         </div>
 
       </div>
@@ -29,13 +29,13 @@ function NavBar() {
       <div className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-5"></div>
 
         <button className="setting-btn mt-1 mr-5">
-          <FontAwesomeIcon className='text-4xl text-[#436299]' icon={faGear} />
+          <FontAwesomeIcon className='text-4xl text-[#436299] transition duration-100 active:scale-90' icon={faGear} />
         </button>
 
         <div className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-5"></div>
 
         <button className="home-btn mt-1 mr-8">
-          <FontAwesomeIcon className='text-4xl text-[#436299]' icon={faHouse} />
+          <FontAwesomeIcon className='text-4xl text-[#436299] transition duration-100 active:scale-90' icon={faHouse} />
         </button>
 
       </div>      
