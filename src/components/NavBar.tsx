f2b31c5d9accead536827d11fa7582755616b6a9
dya-code <dyacode@proton.me>
2022-12-25 09:57:58 +0900

    add music play button & pv video

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,45 @@
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faHouse, faGear } from '@fortawesome/free-solid-svg-icons'
 
 import nav from '../assets/navbar.png'
+import home from '../assets/home.png'
 
 function NavBar() {
 
   return (
-    <div className="navbar flex items-center justify-between fixed bg-[#edf8fd] rounded-b-4xl h-12 w-screen mb-12">
-        
-      <img className="h-[60px]" src={ nav } alt="" />
+    <div className="navbar flex items-center justify-between tracking-tight fixed bg-[#f2fafd] shadow-lg rounded-b-4xl h-[60px] w-screen mb-12">
+      
+      <div className="front-menu flex items-center">
+        <img className="h-[60px]" src={ nav } alt="" />
 
-      <div className="first fixed flex items-center justify-center">
-        <button className="prev bg-[#456399] rounded-full w-[60px] h-[60px] m-10 mt-[70px] transition duration-300 hover:opacity-70">
-          <FontAwesomeIcon className='text-4xl text-white' icon={faArrowLeft} />
+        <div className="first fixed flex items-center justify-center">
+          <button className="prev z-10 bg-[#456399] rounded-full w-[70px] h-[70px] m-10 mt-[85px] transition duration-300 hover:opacity-70">
+            <FontAwesomeIcon className='text-5xl text-white' icon={faArrowLeft} />
+          </button>
+
+          <div className="title text-[#2c4663] tracking-tight text-4xl border-b-4 h-[45px] -ml-12 mt-3 pl-8 border-[#ffe03d]">학생 모집</div>
+          <button className="help text-white bg-[#2a4566] font-bold text-xl w-8 h-8 ml-2 mt-2 rounded-md transition duration-300 hover:opacity-70">?</button>
+        </div>
+
+      </div>
+ 
+      <div className="back-menu flex items-center justify-end">
+
+      <div className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-5"></div>
+
+        <button className="setting-btn mt-1 mr-5">
+          <FontAwesomeIcon className='text-4xl text-[#436299]' icon={faGear} />
         </button>
 
-        <div className="title text-[#2c4663] text-2xl border-b-4 h-[39px] mt-2 border-[#ffe03d]">학생 모집</div>
-          <button className="help text-white bg-[#2a4566] w-6 h-6 ml-2 rounded-md transition duration-300 hover:opacity-70">?</button>
-        </div>
-        
+        <div className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-5"></div>
+
+        <button className="home-btn mt-1 mr-8">
+          <FontAwesomeIcon className='text-4xl text-[#436299]' icon={faHouse} />
+        </button>
+
+      </div>      
+      
     </div>
   )
 }
--- src/views/PickUp.tsx
+++ src/views/PickUp.tsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic } from '@fortawesome/free-solid-svg-icons'
 
 import Atsuko_bg from "../assets/atsuko.png"
 
+import connected_sky from "./connected_sky.wav"
+import Atsuko_PV from "./atsuko_bg.mp4"
+
 function PickUp() {
+  const [isMusic, setIsMusic] = useState(false)
+
+  const onMusic = () => {
+    setIsMusic(true)
+  }
 
   return (
     <div className="pick-up bg-cover font-molu">
-        <img className='atsuko-bg absolute -z-10' src={ Atsuko_bg } />
+
+      { isMusic == true ? 
+        <audio loop autoPlay>
+          {/* <source src={ connected_sky } type="audio/wav" /> */}
+          <source src={ connected_sky } type="audio/wav" />
+        </audio>
+        : null }
+      
+      <video className="w-screen absolute -z-20" muted autoPlay loop>
+        <source src={ Atsuko_PV } type="video/mp4" />
+      </video>
+
+      <div className="flex justify-end">
+        <button className='fixed mr-4 mt-20 bg-white p-3 w-12 h-12 flex justify-center items-center drop-shadow-xl rounded-lg' onClick={onMusic}>
+          <FontAwesomeIcon className='text-3xl text-[#436299]' icon={faMusic} />
+        </button>
+      </div>
+
     </div>
   )
 }
