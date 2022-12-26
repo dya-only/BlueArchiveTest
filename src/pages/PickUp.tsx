a9013f65c2a4f372203940dc15c04f032f335c85
dya-code <dyacode@proton.me>
2022-12-26 22:20:52 +0900

    bug edit

--- src/pages/PickUp.tsx
+++ src/pages/PickUp.tsx
@@ -47,6 +47,7 @@ function PickUp() {
 
       <img className='w-screen h-screen fixed -z-20' src={ Sub_bg } alt="" />
 
+      {/* Gacha Act */}
       { is10P == true || is1P == true ?
         <video className="w-screen overflow-none absolute z-30" autoPlay>
           <source src={ Act10 } type="video/mp4" />
@@ -74,60 +75,62 @@ function PickUp() {
       : null }
 
       {/* Pick Up Window */}
-      <div className="flex justify-end items-center w-screen h-screen text-[#383f46]">
-        <div className="window-bg fixed z-20 w-[680px] h-[580px] bg-[#a9e0f5] rounded-md shadow-md mr-8 mt-[50px]">
-          <div className="pt-3 pb-3 flex justify-center items-center w-[170px] m-[8px] bg-[#f1fbfc] rounded-t-md font-molu text-[23px]">픽업 모집</div>
-          
-          <div className="info w-[680px] h-[445px] -mt-[12px] bg-[#f1fbfc]">
-            <div className="date font-molu text-[23px] pb-6 pt-4 pl-8">2023/01/17 점검 후 ~ 2023/01/31 12:00까지</div>
+      { is10P == false ?
+        <div className="flex justify-end items-center w-screen h-screen text-[#383f46]">
+          <div className="window-bg fixed z-20 w-[680px] h-[580px] bg-[#a9e0f5] rounded-md shadow-md mr-8 mt-[50px]">
+            <div className="pt-3 pb-3 flex justify-center items-center w-[170px] m-[8px] bg-[#f1fbfc] rounded-t-md font-molu text-[23px]">픽업 모집</div>
+            
+            <div className="info w-[680px] h-[445px] -mt-[12px] bg-[#f1fbfc]">
+              <div className="date font-molu text-[23px] pb-6 pt-4 pl-8">2023/01/17 점검 후 ~ 2023/01/31 12:00까지</div>
 
-            <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8 -mt-6"></div>
+              <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8 -mt-6"></div>
 
-            <div className="pickup-text text-[#285986] text-[53px] ml-8 mt-2 mb-1">픽업 모집!</div>
+              <div className="pickup-text text-[#285986] text-[53px] ml-8 mt-2 mb-1">픽업 모집!</div>
 
-            <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>
+              <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>
 
-            <div className="up-text font-molu text-[23px] ml-8 mt-2 mb-1 text-[#107be3]">아츠코(★3) / 아즈사(★3) 출현 확률 UP!</div>
-          
-            <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>
+              <div className="up-text font-molu text-[23px] ml-8 mt-2 mb-1 text-[#107be3]">아츠코(★3) / 아즈사(★3) 출현 확률 UP!</div>
+            
+              <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>
 
-            <div className="sub-notice pl-8 p-2">
-              <div className="sub-notice-text font-molu text-[21px] text-[#24405d]">10회 모집 시, ★2 이상 학생 1명 확정!</div>
-              <div className="sub-notice-text font-molu text-[21px] text-[#24405d] -mt-1">※ 이미 모집한 학생은 엘리그마와 해당 학생의 엘레프로 변환됩니다.</div>
+              <div className="sub-notice pl-8 p-2">
+                <div className="sub-notice-text font-molu text-[21px] text-[#24405d]">10회 모집 시, ★2 이상 학생 1명 확정!</div>
+                <div className="sub-notice-text font-molu text-[21px] text-[#24405d] -mt-1">※ 이미 모집한 학생은 엘리그마와 해당 학생의 엘레프로 변환됩니다.</div>
+              </div>
+
+              {/* Gacha Btn */}
+              <div className="btns flex justify-center items-center mt-12">
+
+                {/* 1회 */}
+                <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick1P }>
+                  <img className="w-[300px] h-[101px]" src={ Btn_1P } alt="" />
+                </button>
+
+                {/* 10회 */}
+                <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick10P }>
+                  <img className="w-[318px] " src={ Btn_10P } alt="" />
+                </button>
+                {/* <Link to="/d10">
+                </Link> */}
+              </div>
+  
             </div>
 
-            {/* Gacha Btn */}
-            <div className="btns flex justify-center items-center mt-12">
+            <div className="under w-[680px] h-[70px] flex items-center justify-center mt-1">
+              <img className="w-[255px] saturate-150 mr-12" src={ Point_200 } />
+              <div className='point flex justify-center'>
+                <img className='h-[47px] -mt-2 saturate-150 ml-2 absolute' src={ PointBack } />
+                <div className="point-text text-[#fdfdff] font-molu-bold ml-2 z-10 text-3xl">30</div>
+              </div>
 
-              {/* 1회 */}
-              <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick1P }>
-                <img className="w-[300px] h-[101px]" src={ Btn_1P } alt="" />
-              </button>
-
-              {/* 10회 */}
-              <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick10P }>
-                <img className="w-[318px] " src={ Btn_10P } alt="" />
-              </button>
-              {/* <Link to="/d10">
-              </Link> */}
+              <button className='ml-[70px] transition duration-100 active:scale-90'>
+                  <img className="h-[56px] -mt-[5px]" src={ SelectBtn } alt="" />
+                </button>
             </div>
- 
+
           </div>
-
-          <div className="under w-[680px] h-[70px] flex items-center justify-center mt-1">
-            <img className="w-[255px] saturate-150 mr-12" src={ Point_200 } />
-            <div className='point flex justify-center'>
-              <img className='h-[47px] -mt-2 saturate-150 ml-2 absolute' src={ PointBack } />
-              <div className="point-text text-[#fdfdff] font-molu-bold ml-2 z-10 text-3xl">30</div>
-            </div>
-
-            <button className='ml-[70px] transition duration-100 active:scale-90'>
-                <img className="h-[56px] -mt-[5px]" src={ SelectBtn } alt="" />
-              </button>
-          </div>
-
         </div>
-      </div>
+        : null }
 
     </div>
   )
